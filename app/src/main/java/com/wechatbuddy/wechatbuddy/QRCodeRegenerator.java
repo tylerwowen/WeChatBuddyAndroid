8d7af479f9251b7fc40d1e4698633b91e825bf98
tyler@mbp <ouyang@cs.ucsb.edu>
2015-08-30 11:59:39 -0700

    Implemented QRCodeRegenerator

--- app/src/main/java/com/wechatbuddy/wechatbuddy/QRCodeRegenerator.java
+++ app/src/main/java/com/wechatbuddy/wechatbuddy/QRCodeRegenerator.java
@@ -1,16 +1,29 @@
 package com.wechatbuddy.wechatbuddy;
 
 import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.ReaderException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 /**
  * Created by Kenneth on 8/29/2015.
  */
-public class QRCodeRegenerator {
-    private Bitmap image;          //UIImage *image
-    private String data;            //NSString *data
+public class QRCodeRegenerator extends  Object{
+    private Bitmap image = null;          //UIImage *image
+    private String data = null;            //NSString *data
 
     public QRCodeRegenerator(){
-
+        super();
     }
 
     /*
@@ -20,11 +33,46 @@ public class QRCodeRegenerator {
     byte[] bitmapdata = blob.toByteArray();
     */
 
-    public Bitmap regenerateQRCodeWithUIImage(Bitmap inputIMG) {
+    public Bitmap regenerateQRCodeWithBitmap(Bitmap inputIMG) {
         this.image = inputIMG;
         this.decodeOriginalImage();
         this.encodeQRCode();
 
         return this.image;
     }
+
+    private void decodeOriginalImage() {
+
+        int[] intArray = new int[image.getWidth()*image.getHeight()];
+        //copy pixel data from the Bitmap into the 'intArray' array
+        image.getPixels(intArray, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+
+        LuminanceSource source = new RGBLuminanceSource(image.getWidth(), image.getHeight(), intArray);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        QRCodeReader reader = new QRCodeReader();
+
+        try {
+            Result result = reader.decode(bitmap);
+            this.data = result.getText();
+        }
+        catch(ReaderException e) {
+            Log.e("DecodingImageERROR", e.getMessage());
+        }
+    }
+
+    private void encodeQRCode() {
+
+        if(this.data != null) {
+            this.image = null;
+            return;
+        }
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix matrix = writer.encode(this.data, BarcodeFormat.QR_CODE, 116, 116);
+        }
+        catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
